a1a7c83f58fe6ce14fbe2f386b726011790b2b1f
iamajeshrana <iamajeshrana4u@gmail.com>
2023-08-29 16:30:54 +0530

    data validation jupyter and modular created

--- src/mlproject/config/configuration.py
+++ src/mlproject/config/configuration.py
@@ -1,6 +1,7 @@
 from mlproject.constants import * # this code help the all importing
 from mlproject.utils.common import read_yaml, create_directories # this code help the read the yaml file and with create directories
 from mlproject.entity.config_entity import DataIngestionConfig
+from mlproject.entity.config_entity import DataValidationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -31,4 +32,20 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
       
